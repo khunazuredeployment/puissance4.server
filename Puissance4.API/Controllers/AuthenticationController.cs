5f96a6ce2953746072feba731a71611db07f0cda
khun ly <khun.ly@bstorm.be>
2025-06-30 22:40:20 +0200

    add log

--- Puissance4.API/Controllers/AuthenticationController.cs
+++ Puissance4.API/Controllers/AuthenticationController.cs
@@ -8,14 +8,10 @@ using System.Security.Authentication;
 namespace Puissance4.API.Controllers
 {
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(AuthenticationService authenticationService, ILogger<AuthenticationController> logger) : ControllerBase
     {
-        private readonly AuthenticationService _authenticationService;
-
-        public AuthenticationController(AuthenticationService authenticationService)
-        {
-            _authenticationService = authenticationService;
-        }
+        private readonly AuthenticationService _authenticationService = authenticationService;
+        private readonly ILogger<AuthenticationController> _logger = logger;
 
         [HttpPost("api/login")]
         public async Task<IActionResult> Login(LoginDTO dto)
@@ -27,6 +23,11 @@ namespace Puissance4.API.Controllers
             catch(AuthenticationException)
             {
                 return BadRequest("Bad Request");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return Problem(ex.Message);
             }
         }
 
@@ -42,9 +43,14 @@ namespace Puissance4.API.Controllers
             {
                 return BadRequest(new ValidationErrorDTO(ex));
             }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return Problem(ex.Message);
+            }
         }
 
-        [HttpGet]
+        [HttpGet("api/test")]
         public async Task<int> Get()
         {
             return await Task.FromResult(42);
